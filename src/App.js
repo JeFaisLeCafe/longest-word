dfc5ea94844e9dc1027f446227b427e84617c2ff
Pierre-Etienne Soury <pierretienne.soury@gmail.com>
2020-02-09 17:24:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Vous voici dans le challenge du mot le plus long</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Votre mission: concaténer des mots afin de créer le plus long mot
+          possible.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input />
       </header>
     </div>
   );
